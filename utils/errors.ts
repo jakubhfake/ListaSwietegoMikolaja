e4074e614f40523df603597336df0a8308c77041
Jakub Haliniak <jakubh.fake@gmail.com>
2023-11-19 18:41:08 +0100

    working on deleting gift from list validation

--- utils/errors.ts
+++ utils/errors.ts
@@ -19,7 +19,7 @@ export class ValidationError extends Error {}
 
         res
             .status(err instanceof ValidationError ? 400 : 500)
-            .render('errors/error', {
-                errorMessage: err instanceof ValidationError ? err.message : 'Przepraszamy, spróbuj ponownie za kilka minut',
+            .json( {
+                errorMessage: err instanceof ValidationError ? err.message : 'Sorry, please try again later.',
             });
     };
